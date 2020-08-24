93ae9fe7fa7f857c45b0d9cddd4b89f188cbde05
Wayter <wayter.paulo.95@gmail.com>
2020-08-24 00:03:14 -0300

    create decks

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,9 +4,30 @@ import Header from '../../components/Header';
 
 import './styles.css';
 const Home = () => {
-    return(
+    return (
         <div id="page-home">
-            <Header/>
+            <Header />
+            <div className="content">
+                <div className="deck">
+                    <div className="init">
+                        <button>Embaralhar</button>
+                        <div className="deck-content">Teste</div>
+                    </div>
+                    <div className="show-cards">
+                        <div className="deck-content">Teste</div>
+                        <button>Virar uma carta</button>
+                    </div>
+                </div>
+                <div className="players">
+                    <h1>Player 1</h1>
+                    <h1>Player 2</h1>
+                    <h1>Player 3</h1>
+                    <h1>Player 4</h1>
+                </div>
+                <div className="scoreboard">
+                    <h1>Placar</h1>
+                </div>
+            </div>
         </div>
     )
 }
